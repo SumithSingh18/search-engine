fb0052e7b710362fff917717a7c0fd795a3e6ed3
Sumith Singh <72908644+SumithSingh18@users.noreply.github.com>
2021-11-25 14:04:29 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# search-engine
+This is the main repository for the Internship Assignment.
+
+I have made a search engine using ExpertRec API which is used for making search boxes for Websites and Web Apps.
+
+Made by Sumith Singh.
+
+
---
+++ scripts.js
@@ -0,0 +1,10 @@
+  (function() {
+    var id = 'cc7e0324-48d4-11ec-a635-0242ac130002';
+    var ci_search = document.createElement('script');
+    ci_search.type = 'text/javascript';
+    ci_search.async = true;
+    ci_search.src = 'https://cse.expertrec.com/api/js/ci_common.js?id=' + id;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ci_search, s);
+  })();
+
